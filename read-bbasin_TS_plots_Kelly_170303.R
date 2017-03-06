118a3215164702ed96a06d76db00cd0d95159c58
Cogswell <CogswellA@dfo-mpo.ca>
2017-03-06 09:54:30 -0400

    Added sackville water levels

--- read-bbasin_TS_plots_Kelly_170303.R
+++ read-bbasin_TS_plots_Kelly_170303.R
@@ -1,30 +1,51 @@
 library(oce)
 
-if (!length(ls(pattern="dir"))) {
+if (!interactive())
+    pdf("bedford_basin.pdf", pointsize=9)
+all <- FALSE
+
+## cache so we can run this locally, e.g. playing with
+## plotting parameters, without waiting for downloads
+
     url <- "ftp://ftp1.dfo-mpo.gc.ca/BIOWebMaster/BBMP/ODF/2017"
     dir <- read.csv(paste(url, "2017667ODFSUMMARY.tsv", sep="/"), skip=2)
-    #dir <- tail(dir$FILE, 9)
-    print("These are the last 9 (or less) files of the current year:")
     print(dir)
-    n <- length(dir$FILE)
+    filenames <- dir$FILE
+    n <- length(filenames)
+    if (!all && n > 9)
+        filenames <- tail(filenames, 9)
     ctds <- vector("list", n)
-    for (i in 1:n) {
+    for (i in seq_along(filenames)) {
         file <- paste(url, dir$FILE[i], sep="/")
         print(file)
         ctds[[i]] <- read.oce(file)
     }
+    S <- unlist(lapply(ctds, function(ctd) c(ctd[["salinity"]], NA)))
+    T <- unlist(lapply(ctds, function(ctd) c(ctd[["temperature"]], NA)))
+    p <- unlist(lapply(ctds, function(ctd) c(ctd[["pressure"]], NA)))
+    
+    Slim <- range(S)
+    Tlim <- range(T)
+    CTD <- as.ctd(S, T, p)
+    par(mfrow=c(3,3))
+    for (ctd in ctds) {
+      plotTS(CTD, col='gray', type='l')
+      lines(ctd[["salinity"]], ctd[["theta"]])
+      mtext(ctd[["startTime"]], line=1, adj=1, cex=0.5)
 }
-S <- unlist(lapply(ctds, function(ctd) c(ctd[["salinity"]], NA)))
-T <- unlist(lapply(ctds, function(ctd) c(ctd[["temperature"]], NA)))
-p <- unlist(lapply(ctds, function(ctd) c(ctd[["pressure"]], NA)))
 
-Slim <- range(S)
-Tlim <- range(T)
-CTD <- as.ctd(S, T, p)
-par(mfrow=c(3,3))
+par(mfrow=c(1, 1))
+# separate graph: water level in Sackville River
+d <- read.csv("http://dd.weather.gc.ca/hydrometric/csv/NS/daily/NS_01EJ004_daily_hydrometric.csv")
+t <- as.POSIXct(d$Date)
+eta <- d$Water.Level...Niveau.d.eau..m.
+oce.plot.ts(t, eta, type='p', drawTimeRange=FALSE)
 for (ctd in ctds) {
-    plotTS(CTD, col='gray', type='l')
-    lines(ctd[["salinity"]], ctd[["theta"]])
-    mtext(ctd[["startTime"]], line=1, adj=1, cex=0.5)
+    ctdTime <- ctd[["startTime"]]
+    abline(v=ctdTime, col="darkgray")
+    mtext(ctdTime, line=0, at=ctdTime)
 }
+
+if (!interactive())
+    dev.off()
 

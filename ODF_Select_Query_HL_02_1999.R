be7cd2cd6959124bf3059035b42c7a1731dee794
Cogswell <CogswellA@dfo-mpo.ca>
2017-10-27 14:51:01 -0300

    Modified ODF select query specifically designed to look for HL2 ODF files in the ODF archive.

---
+++ ODF_Select_Query_HL_02_1999.R
@@ -0,0 +1,229 @@
+# Sections will be run in the order presented:
+# shift+alt+o to expand all sections
+# shift+alt+l to expand current section
+# alt+o to collapse all sections
+# alt+l to collapse current section
+
+#### packages and libraries (more than is necessary but just in case I need them for future development) ----
+
+#if (!require('oce')) devtools::install_github('dankelley/oce')
+#if (!require('dplyr')) install.packages("dplyr")
+#if (!require("rgdal")) install.packages("rgdal")
+#if (!require("dismo")) install.packages("dismo")
+#if (!require("raster")) install.packages("raster")
+#if (!require("maptools")) install.packages("maptools")
+#if (!require("rgeos")) install.packages("rgeos")
+#if (!require("mapview")) install.packages("mapview")
+#if (!require("shiny")) install.packages("shiny")
+#if (!require('devtools')) install.packages('devtools')
+#if (!require('htmltools')) install.packages('htmltools')
+#if (!require('leaflet')) devtools::install_github('rstudio/leaflet')
+
+
+library(oce)
+library(ocedata)
+library(rgdal)
+library(dismo)
+library(raster)
+library(maptools) 
+library(rgeos)
+library(mapview)
+library(leaflet)
+library(dplyr)
+library(htmltools)
+library(htmlwidgets)
+library(tools)
+library(lubridate)
+
+#### Input data to run scrip: Search Area Name, Extent, Start and End Years, Data Output Directories----
+
+sl<-"HL02" #Enter the name for your search area
+
+# Enter your search area extent taken from the HL2 Retrieval Area from the AZMP website
+maxn<-44.37
+minn<-44.17
+maxw<-(-63.42)
+minw<-(-63.22)
+
+# Enter the start and end years of your ODF search from the ODF archive (current year is likely excluded from the database)
+ys<-1999
+ye<-2016
+range<-c(ys:ye)
+
+# Enter your output working directory
+fwd<-("C:\\Users\\CogswellA\\Documents\\test\\") #establisheds the working directory for the meta-data summary export
+setwd(fwd)
+
+#### select all ODF files into list from folders in archive ----
+plist<-list() #creates empty list to accept the filtered ODF files
+mdata_all_years<-NULL
+is.not.null <- function(x) ! is.null(x)
+
+for (i in min(range):max(range)){
+  
+  #wd<-paste("C:\\Users\\CogswellA\\Documents\\AZMP\\Data Management\\2017\\ODFArchive_1999_2016\\",i,sep="")
+  wd<-paste("R:\\Science\\BIODataSvc\\ARC\\Archive\\ctd\\",i,sep="")
+  setwd(wd) #working directory is the year within the ctd archive folder within the loop
+  dn_ODF_files <- list.files(pattern="*^.*DN.*.ODF$") # only selects down casts using the DN in the file name
+  #if (i!= ys) {select_ODF_files<-dn_ODF_files[-grep("BCD", dn_ODF_files, fixed=T)]} else {select_ODF_files<-dn_ODF_files} 
+  select_ODF_files<-dn_ODF_files
+  
+  #this loop pulls all odf files from the folder and spatially filters and selects them into ylist
+  fname=NULL
+  mdata<-NULL
+  
+  for (n in 1:length(select_ODF_files)){
+    tmp_odf<-read.odf(select_ODF_files[n])
+    #tmp_odf<-test
+    tmp_mdata<-NULL
+    
+    if(tmp_odf[['latitude']]<=maxn & tmp_odf[['latitude']]>=minn & tmp_odf[['longitude']]>=maxw & tmp_odf[['longitude']]<=minw & is.not.null(tmp_odf[['conductivity']])) x<-1 else x<-0
+    
+    if (x==1) {
+      fname<-as.data.frame(tmp_odf@metadata$filename)
+      stime<-as.data.frame(tmp_odf@metadata$startTime)
+      ship<-as.data.frame(tmp_odf@metadata$ship)
+      lon<-as.data.frame(tmp_odf@metadata$longitude)
+      lat<-as.data.frame(tmp_odf@metadata$latitude)
+      tmp_mdata<-cbind(fname,stime,ship,lon,lat)
+      names(tmp_mdata)<-c("filename","starttime","ship","longitude","latitude")
+            }
+    
+    mdata<-rbind(mdata,tmp_mdata)
+    
+  }
+  
+  #proc.time()
+ 
+  
+  mdata_all_years<-rbind(mdata,mdata_all_years)
+   
+}
+
+
+mdata_all_years$starttime<-ymd_hms(mdata_all_years$starttime, tz="America/Halifax")
+mdata_all_years$year<-substr(mdata_all_years$starttime,1,4)
+
+#### export file metadata and list of odf files ----
+
+# Enter your output working directory
+setwd(fwd)
+
+
+dataout<-(fwd) #output path for the ODF's to be copied from the archive.
+fname_csv<-paste(sl,"_",ys,"_",ye,"_ODFSummary_",as.numeric(format(Sys.Date(), "%Y%m%d")),".csv",sep="") #file name for your csv output
+write.csv(mdata_all_years,fname_csv, row.names=F) #save your file meta data in .csv format in your wd
+#fname_rds<-paste(sl,"_",ys,"_",ye,"_ODFList_",as.numeric(format(Sys.Date(), "%Y%m%d")),".rds",sep="") #file name for your odf list output
+#saveRDS(plist,fname_rds) #save your odf data in list form for easy access in R without compiling
+
+#These portions are useful if you do not want to compile these files again
+mdata_all_years<-read.csv("HL02_1999_2016_ODFSummary_20171018.csv") #load your metadata
+mdata_all_years<-dplyr::arrange(mdata_all_years,starttime)
+#plist<-readRDS(fname_rds) #load your odf list for viewing below
+
+#### plot extent of selections and view metadata----
+
+AZMPcore <- readOGR("C:\\Users\\CogswellA\\Documents\\AZMP\\Missions\\ArcGIS Projects\\AZMP_PMZA_Coordinates.shp")
+AZMPcore<- AZMPcore@data
+AZMPcore_fixed<-subset(AZMPcore, (!is.na(AZMPcore$station_na)))
+AZMPcore_section<-subset(AZMPcore, (is.na(AZMPcore$station_na)))
+
+odfmap<-leaflet(mdata_all_years) %>%
+  fitBounds(min(mdata_all_years$longitude),min(mdata_all_years$latitude),max(mdata_all_years$longitude),max(mdata_all_years$latitude)) %>%
+  addTiles(urlTemplate = 'http://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', 
+           attribution = 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC')%>%  # Add awesome tiles
+  addCircles(lng=mdata_all_years$longitude,lat=mdata_all_years$latitude, weight = 5, radius=10, color="black", stroke = TRUE,opacity=0.5,group="CTD Subset Locations",
+             fillOpacity = 1,popup=paste ("File Name:",mdata_all_years$filename,"|", "Start Time:", mdata_all_years$starttime,"|","Lon DD:", round(mdata_all_years$longitude,3), "|","Lat DD:",round(mdata_all_years$latitude,3),"|","Depth m:",mdata_all_years$depth,sep=" "))%>%
+  addCircles(lng=AZMPcore_section$lon_dd,lat=AZMPcore_section$lat_dd, weight = 10, radius=10, color="yellow", stroke = TRUE,opacity=0.5,group="AZMP Core Section Stations",
+             fillOpacity = 1,popup=paste ("Section Name:",AZMPcore_section$section_na,"|", "Station Name:", AZMPcore_section$section_st,"|","Lon DD:", AZMPcore_section$lon_dd, "|","Lat DD:",AZMPcore_section$lat_dd,"|","Depth m:",AZMPcore_section$depth_m,sep=" "))%>%
+  addCircles(lng=AZMPcore_fixed$lon_dd,lat=AZMPcore_fixed$lat_dd, weight = 10, radius=10, color="Red", stroke = TRUE,opacity=0.5,group="AZMP Fixed Stations",
+             fillOpacity = 1,popup=paste ("Station Name:",AZMPcore_fixed$station_na,"|", "Lon DD:", AZMPcore_fixed$lon_dd, "|","Lat DD:",AZMPcore_fixed$lat_dd,"|","Depth m:",AZMPcore_fixed$depth_m,sep=" "))%>%
+  addLabelOnlyMarkers(lng=AZMPcore_section$lon_dd, lat=AZMPcore_section$lat_dd,label =  as.character(AZMPcore_section$section_st),group="Section Labels", 
+                      labelOptions = labelOptions(noHide = T, direction = 'top', textOnly = T))%>%
+  addLabelOnlyMarkers(lng=AZMPcore_fixed$lon_dd, lat=AZMPcore_fixed$lat_dd,label =  as.character(AZMPcore_fixed$station_na),group="Fixed Station Labels", 
+                      labelOptions = labelOptions(noHide = T, direction = 'top', textOnly = T))%>%
+  addLegend("bottomright", colors= c("black", "yellow", "red"), labels=c("CTD Locations","AZMP Core Section Stations","AZMP Fixed Stations"), title=paste("Map of ODF files near ", sl, " created on ",Sys.Date(), sep=""),opacity=1)%>% 
+  addScaleBar("bottomleft",options=scaleBarOptions(maxWidth=100,imperial=T,metric=T,updateWhenIdle=T))%>%
+  addLayersControl(
+    overlayGroups = c("CTD Subset Locations", "AZMP Core Section Stations", "AZMP Fixed Stations", "Section Labels", "Fixed Station Labels"),
+    options = layersControlOptions(collapsed = TRUE)
+  )
+
+odfmap
+
+odf_html<-paste(sl,"_",ys,"_",ye,"_ODFmap_",as.numeric(format(Sys.Date(), "%Y%m%d")),".html",sep="")
+
+saveWidget(odfmap,odf_html)  
+
+#### copy selected ODF files to specified directory and zip them ----
+
+
+mdata_all_years$year<-substr(mdata_all_years$starttime,1,4)
+yl<-unique(mdata_all_years$year)
+
+for (y in 1:length(yl)){
+  
+  mdataloop<-subset(mdata_all_years, mdata_all_years$year==yl[y])
+  ml<-nrow(mdataloop)
+
+ for (c in 1:ml){
+  
+  file.copy(mdataloop$filename[c],paste(dataout,"ODF\\",yl[y],"\\",sep=""),overwrite = T, recursive=F)
+   fname<-paste("ODFSUMMARY","_",yl[y],".tsv",sep="")
+   odf_summary_file <- paste(dataout,"ODF\\",yl[y],"\\",fname, sep = "")
+   cat(paste("Folder consists of ", 
+             ml,
+             " ODF files near ",
+             sl,
+             ".",
+             sep=""), 
+       file = odf_summary_file, sep = "\n", append = FALSE)
+   cat("", file = odf_summary_file, sep = "\n", append = TRUE)
+   write.table(mdataloop[,c(1:3,5:6)], file = odf_summary_file, append = TRUE, quote = TRUE, sep=",",
+               eol = "\n", na = "NA", dec = ".", row.names = FALSE, col.names = TRUE)
+  
+ }
+
+}
+
+#### copy selected ODF files to specified directory and convert to .csv files ----
+
+
+fwd<-("C:\\Users\\CogswellA\\Documents\\HL2\\") #establisheds the working directory for the meta-data summary export
+setwd(fwd)
+mdata_all_years<-read.csv("HL02_1999_2016_ODFSummary_20171018.csv")
+mdata_all_years$year<-substr(mdata_all_years$starttime,1,4)
+yl<-unique(mdata_all_years$year)
+
+for (y in 1:length(yl)){
+  
+  
+  mdataloop<-subset(mdata_all_years, mdata_all_years$year==yl[y])
+  ml<-nrow(mdataloop)
+  
+  for (c in 1:ml){
+    
+    ctd_c<-read.ctd.odf(as.character(mdataloop$filename[c]))
+    write.ctd(ctd_c, paste(fwd,"CSV\\",yl[y],"\\",tools::file_path_sans_ext(basename(ctd_c[['filename']])),".csv",sep=""),flags=F)
+    
+  }
+  
+  fname<-paste("CSVSUMMARY","_",yl[y],".tsv",sep="")
+  csv_summary_file <- paste(dataout,"CSV\\",yl[y],"\\",fname, sep = "")
+  cat(paste("Folder consists of ", 
+            ml,
+            " CSV files near HL2",
+            ".",
+            sep=""), 
+      file = csv_summary_file, sep = "\n", append = FALSE)
+  cat("", file = csv_summary_file, sep = "\n", append = TRUE)
+  write.table(mdataloop[,c(1:3,5:6)], file = csv_summary_file, append = TRUE, quote = TRUE, sep=",",
+              eol = "\n", na = "NA", dec = ".", row.names = FALSE, col.names = TRUE)
+  
+}
+
+
+
+
+#files2zip<-dir(dataout,full.names=T)
+#zip(zipfile='ODFzip',files=files2zip)
